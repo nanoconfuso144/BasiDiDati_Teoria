571ef1376567b228c9817bab8f02a9c466ff0702
nanoconfuso144 <luca.vezzoli1@studenti.unimi.it>
2023-04-18 14:33:49 +0200

    query correlate

---
+++ lez_13/2023_04_18.sql
@@ -0,0 +1,25 @@
+-- selezionare i film che non sono stati distribuiti nei paesi nei quali sono stati prodotti
+-- produced(movie,country) released(movie,country)
+
+--soluzione 1
+--sottraggo all'insieme dei film l'insieme dei film distribuiti in almeno un paese di produzione
+select id 
+from imdb.movie
+except
+select distinct produced.movie
+from imdb.produced, imdb.released 
+where produced.movie = released.movie and produced.country = released.country;
+
+--soluzione 2
+--operatore EXISTS. da utilizzare nella clauso where con argomento una sottoquery. vero se restituisce almeno un risultato, falso se non restituisce alcun risultato
+-- un film è nel risultato se non esiste un record di produzione per quel film per il quale esiste un record di distibuzione per quel film nel medesimo paese
+select m.id, m.official_title 
+from imdb.movie m 
+where not exists (
+select *
+from imdb.produced p 
+where m.id = p.movie and not exists ( 
+select *
+from imdb.released r
+where p.movie = r.movie and p.country = r.country
+));
