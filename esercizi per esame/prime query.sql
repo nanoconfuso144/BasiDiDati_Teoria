33add11f8d7c82fe3fb36886d637fcfebe0f4ba5
nanoconfuso144 <luca.vezzoli1@studenti.unimi.it>
2023-07-17 11:07:02 +0200

    esercizi

--- esercizi per esame/prime query.sql
+++ esercizi per esame/prime query.sql
@@ -104,4 +104,136 @@ select distinct released.movie
 from imdb.released left join prod_ril on released.movie = prod_ril.movie
 where prod_ril.movie is null;
 
+-- restituire il numero di pellicole per ogni anno disponibile (con ordinamento)
+
+select year, count(*) as "pellicole per anno"
+from  imdb.movie m 
+group by year 
+order by "pellicole per anno" desc;
+
+-- restituire gli anni nei quali ci sono più di 10 film a partire dal 2010
+
+with film_per_anni as(
+select year, count(*) as "pellicole per anno"
+from  imdb.movie m 
+group by year 
+)
+select year
+from film_per_anni
+where "pellicole per anno" > 10;
+
+-- restituire per ciascun film il numero di persone coinvolte per ciascun ruolo
+
+select movie, p_role, count(*), count(person), count(distinct person)
+from imdb.crew
+group by movie, p_role;
+
+-- restituire la durata media delle pellicole per ogni anno (con ordinamento)
+
+select year, avg(length) 
+from imdb.movie
+group by year
+order by year desc;
+
+-- restituire il numero di persone per ruolo
+
+select p_role, count(distinct person) as "numero"
+from imdb.crew
+group by p_role
+order by "numero";
+
+-- restituire per ogni movie il numero di materiali disponibili e il titolo del film
+-- material(id, description, language, movie)
+
+select mat.movie, mov.official_title, count(mat.id) as "materiale",
+from imdb.material mat right join imdb.movie mov on mat.movie = mov.id
+group by mat.movie, mov.official_title
+order by 3 desc;
+
+-- restituire le persone che recitano in più di 10 pellicole
+
+select c.person, count(c.movie) "n.film"
+from imdb.crew c
+where c.p_role = 'actor'
+group by c.person
+having count(c.movie)>10;
+
+-- restituire i film con cast più ampio di 10 attori
+
+select movie, count(person) 
+from imdb.crew c 
+where p_role = 'actor'
+group by movie
+having count(person) > 10 
+order by 2 desc;
+
+-- restituire le persone che non hanno mai recitato in un film
+with hanno_recitato as(
+select person, count(movie) as "film"
+from imdb.crew c 
+where p_role = 'actor'
+group by person
+)
+select p.id, p.given_name
+from imdb.person p left join hanno_recitato h on p.id = h.person
+where h."film" is null;
+
+-- selezionare le persone che hanno recitato in tutti i film di genere Crime
+
+select p.id
+from imdb.person p
+where not exists (
+	select *
+	from imdb.genre g
+	where g.genre = 'Crime' and not exists (
+		select *
+		from imdb.crew c
+		where c.p_role ='actor' and c.movie = g.movie and c.person = p.id 
+	)
+);
+
+-- selezionare i film con cast più numeroso della media dei film del medesimo genere
+with media_generi as (
+select g.genre, count(c.person)/count(distinct c.movie)  as "media"  
+from imdb.genre g inner join imdb.crew c on g.movie = c.movie
+where c.p_role = 'actor'
+group by g.genre
+)
+select distinct g2.movie 
+from media_generi mg inner join imdb.genre g2 on mg.genre = g2.genre
+where mg."media" <= all(
+	select count(distinct c1.person)
+	from imdb.crew c1 
+	where c1.p_role = 'actor' and c1.movie = g2.movie
+	group by movie
+)
+order by g2.movie;
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
